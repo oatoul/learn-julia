77a2d0b17456c57ad88214bbcba91942b1504586
Luo Tao <luotao325@gmail.com>
2021-06-18 18:58:08 +0800

    refactor

--- scripts/boolean_network.jl
+++ scripts/boolean_network.jl
@@ -5,7 +5,7 @@ using DataFrames
 import Cambrian.mutate
 
 
-function evaluate(ind::CGPInd, X::AbstractArray, Y::AbstractArray)
+function evaluate!(ind::CGPInd, X::AbstractArray, Y::AbstractArray)
     accuracy = 0.0
     for i in 1:size(X, 1)
         out = process(ind, collect(X[i]))
@@ -16,7 +16,7 @@ function evaluate(ind::CGPInd, X::AbstractArray, Y::AbstractArray)
     [accuracy / size(X, 1)]
 end
 
-function get_active_connections(ind::CGPInd, low::Int64, high::Int64)
+function get_active_connections!(ind::CGPInd, low::Int64, high::Int64)
     nodes = ind.nodes[[n.active for n in ind.nodes]]
     res = Set()
     for node in nodes
@@ -33,27 +33,16 @@ function get_active_connections(ind::CGPInd, low::Int64, high::Int64)
     res
 end
 
-df = DataFrame(CSV.File("data/CDC15_bool.tsv",drop=["Time"],type=Bool))
-insertcols!(df, 1, :T0 => false, :T1 => true)
-
-ndf = names(df)
-l_idx = 3
-h_idx = size(ndf)[1]
-
-for col in ndf
-    println("$(col)")
-end
-
-function runBN!(e::AbstractEvolution, low::Int64, high::Int64, fitness::Int64)
-    step!(e)
-    while(e.population[end].fitness[1] < fitness && e.gen < e.config.n_gen)
-        step!(e)
-    end
-    log_gen(e)
-    save_gen(e)
-    set = get_active_connections(e.population[end], low, high)
-    println(set)
-end
+# function runBN!(e::AbstractEvolution, low::Int64, high::Int64, fitness::Int64)
+#     step!(e)
+#     while(e.population[end].fitness[1] < fitness && e.gen < e.config.n_gen)
+#         step!(e)
+#     end
+#     log_gen(e)
+#     save_gen(e)
+#     set = get_active_connections(e.population[end], low, high)
+#     println(set)
+# end
 
 function getBN!(df::DataFrame, fitness::Float64)
     ndf = names(df)
@@ -72,7 +61,7 @@ function getBN!(df::DataFrame, fitness::Float64)
         popfirst!(Y)
 
         
-        fit(ind::CGPInd) = evaluate(ind, X, Y)
+        fit(ind::CGPInd) = evaluate!(ind, X, Y)
         e = CGPEvolution(cfg, fit)
 
         step!(e)
@@ -83,7 +72,7 @@ function getBN!(df::DataFrame, fitness::Float64)
         println("Fitness: $(e.population[end].fitness[1])")
         # println(summary(e.population[end]))
 
-        set = get_active_connections(e.population[end], l_idx, h_idx)
+        set = get_active_connections!(e.population[end], l_idx, h_idx)
         res = []
         for j in set
             push!(res, ndf[j])
@@ -94,6 +83,17 @@ function getBN!(df::DataFrame, fitness::Float64)
 
     end    
 end
+
+df = DataFrame(CSV.File("data/CDC15_bool.tsv",drop=["Time"],type=Bool))
+insertcols!(df, 1, :T0 => false, :T1 => true)
+
+ndf = names(df)
+l_idx = 3
+h_idx = size(ndf)[1]
+
+# for col in ndf
+#     println("$(col)")
+# end
 
 cfg = get_config("cfg/CDC15.yaml")
 mutate(ind::CGPInd) = goldman_mutate(cfg, ind)
