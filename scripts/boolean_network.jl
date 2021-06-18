aac6ae7feea1c825489552383cb07d925e4883ea
Luo Tao <luotao325@gmail.com>
2021-06-18 20:52:06 +0800

    save

--- scripts/boolean_network.jl
+++ scripts/boolean_network.jl
@@ -45,6 +45,7 @@ end
 # end
 
 function getBN!(df::DataFrame, fitness::Float64)
+    BN = Set()
     ndf = names(df)
     l_idx = 3
     h_idx = size(ndf)[1]
@@ -73,28 +74,116 @@ function getBN!(df::DataFrame, fitness::Float64)
         # println(summary(e.population[end]))
 
         set = get_active_connections!(e.population[end], l_idx, h_idx)
-        res = []
-        for j in set
-            push!(res, ndf[j])
+
+        "output result for target gene"
+        # res = []
+        # for j in set
+        #     push!(res, ndf[j])
+        # end
+        # println("$(target) is connected with:")
+        # println(res)
+
+        "store connections to BN"
+        for k in set
+            conn = ndf[k] * "_" * target
+            push!(BN, conn)
+            println(conn)
         end
 
-        println("$(target) is connected with:")
-        println(res)
 
-    end    
+    end
+
+    BN
 end
 
-df = DataFrame(CSV.File("data/CDC15_bool.tsv",drop=["Time"],type=Bool))
+"
+(TP + TN) / (TP + FP + FN + TN)
+
+TP (true positive) correctly predicted connections
+FP (false positive) incorrectly predicted connections
+FN (false negative) non-inferred connections
+TN (true negative) correct negative predictions
+
+"
+function get_structural_accuracy(expect::Set, actual::Set, universe::Set)
+    TP = length(intersect(actual, expect))
+    FP = length(setdiff(actual, expect))
+    FN = length(setdiff(expect, actual))
+    expect_negative = setdiff(universe, expect)
+    actual_negative = setdiff(universe, actual)
+    TN = length(intersect(actual_negative, expect_negative))
+
+    acc = (TP + TN) / (TP + FP + FN + TN)
+    println("TP : $(TP)")
+    println("FP : $(FP)")
+    println("FN : $(FN)")
+    println("TN : $(TN)")
+    println("Structural accuracy : $(acc)")
+
+    acc
+end
+
+function get_universe_set(inputs::Any)
+    res = Set()
+    for i in inputs
+        for j in inputs
+            re = i * "_" * j
+            push!(res, re)
+        end
+    end
+    res
+end
+
+function get_expect_CDC15()
+    res = Set()
+    push!(res, "FKH2_SWI5")
+    push!(res, "FKH2_ACE2")
+
+    push!(res, "ACE2_CLN3")
+
+    push!(res, "SWI6_NDD1")
+    push!(res, "SWI6_SWI4")
+
+    push!(res, "NDD1_ACE2")
+    push!(res, "NDD1_SWI5")
+
+    push!(res, "SWI5_CLN3")
+
+    push!(res, "MCM1_ACE2")
+    push!(res, "MCM1_SWI5")
+    push!(res, "MCM1_CLN3")
+    push!(res, "MCM1_SWI4")
+
+    push!(res, "CLN3_SWI4")
+    push!(res, "CLN3_MBP1")
+
+    push!(res, "MBP1_NDD1")
+    push!(res, "MBP1_SWI4")
+
+    push!(res, "SWI4_NDD1")
+
+    res
+end
+
+
+cfg = get_config("cfg/CDC15.yaml")
+mutate(ind::CGPInd) = goldman_mutate(cfg, ind)
+
+df_origin = DataFrame(CSV.File("data/CDC15_bool.tsv",drop=["Time"],type=Bool))
+df = copy(df_origin)
 insertcols!(df, 1, :T0 => false, :T1 => true)
 
-ndf = names(df)
-l_idx = 3
-h_idx = size(ndf)[1]
-
+# ndf = names(df)
+# l_idx = 3
+# h_idx = size(ndf)[1]
 # for col in ndf
 #     println("$(col)")
 # end
 
-cfg = get_config("cfg/CDC15.yaml")
-mutate(ind::CGPInd) = goldman_mutate(cfg, ind)
-getBN!(df, 0.9)
+universe = get_universe_set(names(df_origin))
+
+expect = get_expect_CDC15()
+
+actual = getBN!(df, 0.9)
+
+stru_acc = get_structural_accuracy(expect, actual, universe)
