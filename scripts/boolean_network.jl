efedf73294fb7f78adac7aa955239ccc24942d25
Luo Tao <luotao325@gmail.com>
2021-07-06 17:45:01 +0800

    voting

--- scripts/boolean_network.jl
+++ scripts/boolean_network.jl
@@ -1,7 +1,10 @@
+using Base: String
+using StatsBase: push!, length
 using CartesianGeneticProgramming
 using Cambrian
 using CSV
 using DataFrames
+using StatsBase
 import Cambrian.mutate
 
 
@@ -29,7 +32,15 @@ end
 # end
 
 function getBN!(df::DataFrame, fitness::Float64)
-    BN = Set()
+    BN_9 = Set()
+    BN_8 = Set()
+    BN_7 = Set()
+    BN_6 = Set()
+    BN_5 = Set()
+    BN_4 = Set()
+    BN_3 = Set()
+    BN_2 = Set()
+
     ndf = names(df)
     l_idx = 3
     h_idx = size(ndf)[1]
@@ -62,7 +73,16 @@ function getBN!(df::DataFrame, fitness::Float64)
             println("Fitness $(el.fitness) sparsity $(el.sparsity) active $(el.n_active)")
         end
 
-        set = get_active_connections!(e.elites[end], l_idx, h_idx)
+        # set = get_active_connections!(e.elites[end], l_idx, h_idx)
+        set9 = get_connections_by_vote!(0.9, e.elites, l_idx, h_idx)
+        set8 = get_connections_by_vote!(0.8, e.elites, l_idx, h_idx)
+        set7 = get_connections_by_vote!(0.7, e.elites, l_idx, h_idx)
+        set6 = get_connections_by_vote!(0.6, e.elites, l_idx, h_idx)
+        set5 = get_connections_by_vote!(0.5, e.elites, l_idx, h_idx)
+        set4 = get_connections_by_vote!(0.4, e.elites, l_idx, h_idx)
+        set3 = get_connections_by_vote!(0.3, e.elites, l_idx, h_idx)
+        set2 = get_connections_by_vote!(0.2, e.elites, l_idx, h_idx)
+
 
         "output result for target gene"
         # res = []
@@ -73,17 +93,57 @@ function getBN!(df::DataFrame, fitness::Float64)
         # println(res)
 
         "store connections to BN"
-        for k in set
-            conn = ndf[k] * "_" * target
-            push!(BN, conn)
-            println(conn)
-        end
-
+        # for k in set
+        #     conn = ndf[k] * "_" * target
+        #     push!(BN, conn)
+        #     println(conn)
+        # end
+        store_BN(set9, BN_9, ndf, target)
+        store_BN(set8, BN_8, ndf, target)
+        store_BN(set7, BN_7, ndf, target)
+        store_BN(set6, BN_6, ndf, target)
+        store_BN(set5, BN_5, ndf, target)
+        store_BN(set4, BN_4, ndf, target)
+        store_BN(set3, BN_3, ndf, target)
+        store_BN(set2, BN_2, ndf, target)
 
     end
 
-    BN
+    BN_9, BN_8, BN_7, BN_6, BN_5, BN_4, BN_3, BN_2
 end
+
+
+function store_BN(set::Set, BN::Set, ndf::Array, target::String)
+    for k in set
+        conn = ndf[k] * "_" * target
+        push!(BN, conn)
+        # println(conn)
+    end
+end
+
+
+
+function get_connections_by_vote!(vote_ratio::Float64, elites::Array{CGPInd}, low::Int64, high::Int64)
+    res_set = Set()
+    res_all = []
+    min_vote = length(elites) * vote_ratio
+    println("min vote is $(min_vote)")
+    for e in elites
+        r = collect(get_active_connections!(e, low, high))
+        res_all = vcat(res_all, r)
+    end
+    # println(res_all)
+    count = StatsBase.countmap(res_all)
+    # println(count)
+    for (k,v) in count
+        if v >= min_vote
+            push!(res_set,k)
+        end
+    end
+    # println(res_set)
+    res_set
+end
+
 
 "
 (TP + TN) / (TP + FP + FN + TN)
@@ -173,6 +233,22 @@ universe = get_universe_set(names(df_origin))
 
 expect = get_expect_CDC15()
 
-actual = getBN!(df, 0.99)
+BN_9, BN_8, BN_7, BN_6, BN_5, BN_4, BN_3, BN_2 = getBN!(df, 0.99)
 
-stru_acc = get_structural_accuracy(expect, actual, universe)
+# actual = getBN!(df, 0.99)
+println("BN_9")
+stru_acc9 = get_structural_accuracy(expect, BN_9, universe)
+println("BN_8")
+stru_acc8 = get_structural_accuracy(expect, BN_8, universe)
+println("BN_7")
+stru_acc7 = get_structural_accuracy(expect, BN_7, universe)
+println("BN_6")
+stru_acc6 = get_structural_accuracy(expect, BN_6, universe)
+println("BN_5")
+stru_acc5 = get_structural_accuracy(expect, BN_5, universe)
+println("BN_4")
+stru_acc4 = get_structural_accuracy(expect, BN_4, universe)
+println("BN_3")
+stru_acc3 = get_structural_accuracy(expect, BN_3, universe)
+println("BN_2")
+stru_acc2 = get_structural_accuracy(expect, BN_2, universe)
