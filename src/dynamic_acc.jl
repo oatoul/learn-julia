e86830d6f17b35603d00b8ec3243f479ec4c3f0a
Luo Tao <luotao325@gmail.com>
2021-09-09 16:11:28 +0800

    save

--- src/dynamic_acc.jl
+++ src/dynamic_acc.jl
@@ -1,8 +1,11 @@
 using DataStructures
+using CSV
+using DataFrames
+import Statistics
 
-function gene_wise_consistency(X::Array{String}, Y::Array{Int})
-    arr0 = String[]
-    arr1 = String[]
+function gene_wise_consistency(X::Vector, Y::Array{Int})
+    arr0 = []
+    arr1 = []
 
     len = length(X)
 
@@ -28,8 +31,8 @@ function gene_wise_consistency(X::Array{String}, Y::Array{Int})
 end
 
 
-function dynamic_consistency(df::DataFrame, conns::Array{String})
-    conn_map = Dict{String, Set{String}}
+function get_conn_map(conns::Set)
+    conn_map = Dict{String, Set{String}}()
     for con in conns
         r_t = split(con, "_")
         re = r_t[1]
@@ -37,7 +40,31 @@ function dynamic_consistency(df::DataFrame, conns::Array{String})
         vals = get!(Set{String}, conn_map, ta)
         push!(vals, re)
     end
+    println(conn_map)
+    conn_map
+end
 
+
+function dynamic_consistency(df::DataFrame, conns::Set)
+    conn_map = get_conn_map(conns)
+    gene_consis = []
+    for (ta, res) in conn_map
+        rea = toArray(res)
+        X = copy(Tuple.(eachrow(df[!,rea])))
+        Y = copy(df[!,ta])
+        # t + 1
+        pop!(X)
+        popfirst!(Y)
+
+        g_c = gene_wise_consistency(X, Y)
+        println("$(ta) $(g_c)")
+        push!(gene_consis, g_c)
+    end
+    Statistics.mean(gene_consis)
+end
+
+function toArray(s::Set)
+    (x -> x).(s)
 end
 
 # x = ["100", "001", "111", "100", "011"]
@@ -46,3 +73,39 @@ end
 # css = gene_wise_consistency(x, y)
 
 # println(css)
+
+function get_expect_CDC15()
+    res = Set()
+    push!(res, "FKH2_SWI5")
+    push!(res, "FKH2_ACE2")
+
+    push!(res, "ACE2_CLN3")
+
+    push!(res, "SWI6_NDD1")
+    push!(res, "SWI6_SWI4")
+
+    push!(res, "NDD1_ACE2")
+    push!(res, "NDD1_SWI5")
+
+    push!(res, "SWI5_CLN3")
+
+    push!(res, "MCM1_ACE2")
+    push!(res, "MCM1_SWI5")
+    push!(res, "MCM1_CLN3")
+    push!(res, "MCM1_SWI4")
+
+    push!(res, "CLN3_SWI4")
+    push!(res, "CLN3_MBP1")
+
+    push!(res, "MBP1_NDD1")
+    push!(res, "MBP1_SWI4")
+
+    push!(res, "SWI4_NDD1")
+
+    res
+end
+
+df_origin_bool = DataFrame(CSV.File("data/CDC15_bool.tsv",drop=["Time"],type=Int))
+println(df_origin_bool)
+
+dc = dynamic_consistency(df_origin_bool, get_expect_CDC15())
